8a09a0ad609dd118f6188454f814cbb146ec34db
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-10-06 07:45:52 +0700

    Update play.py

--- KazuMusic/plugins/play.py
+++ KazuMusic/plugins/play.py
@@ -12,7 +12,7 @@ from config import BANNED_USERS, lyrical
 from strings import get_command
 from KazuMusic import (Apple, Resso, SoundCloud, Spotify, Telegram,
                         YouTube, app)
-from KazuMusic.core.call import Anon
+from KazuMusic.core.call import Kazu
 from KazuMusic.utils import seconds_to_min, time_to_seconds
 from KazuMusic.utils.channelplay import get_channeplayCB
 from KazuMusic.utils.database import is_video_allowed
